274f3536e0c47e5f3c0e095ba7625b066110580f
bakaka <mrnursil@gmail.com>
2018-11-19 20:43:53 +0400

    add controller

---
+++ app/controllers/v1/articles_controller.rb
@@ -0,0 +1,11 @@
+module v1
+  class ArticlesController < ActionController::API
+    def index
+      render json: news_items_collection
+    end
+    private
+    def news_items_collection
+      NewsItem.where("LOWER(title) ILIKE '%#{params[:query].downcase}%'")
+    end
+  end
+end
